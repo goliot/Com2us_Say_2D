8fa4378f150af128207d36f600225da280ab2634
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-03-24 13:39:32 +0900

    250324 - 미사용 enum 삭제

--- Assets/02. Scripts/Enums/EEnemyType.cs
+++
@@ -1,8 +0,0 @@
-public enum EEnemyType
-{
-    Basic,
-    Split,
-    Bezier,
-    Target,
-    Follow,
-}
--- Assets/02. Scripts/Enums/EItemType.cs
+++
@@ -1,8 +0,0 @@
-public enum EItemType
-{
-    SpeedUp,
-    AttackSpeedUp,
-    Heal,
-    HpUp,
-    Magnetic,
-}
