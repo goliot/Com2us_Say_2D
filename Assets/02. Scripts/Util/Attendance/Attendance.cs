9bc3bc9efc29e61e914dc99270d5e07557d5a7aa
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-03-31 16:22:52 +0900

    250331 - Attendance

---
+++ Assets/02. Scripts/Util/Attendance/Attendance.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Attendance
+{
+    public readonly AttendanceDataSO Data;
+
+    private bool _isRewarded;
+    public bool IsRewarded
+    {
+        get => _isRewarded;
+        set
+        {
+            _isRewarded  = value;
+        }
+    }
+
+
+    //데이터와 보상 유무
+    public Attendance(AttendanceDataSO data, bool rewarded)
+    {
+        Data = data;
+        _isRewarded = rewarded;
+    }
+}
---
+++ Assets/02. Scripts/Util/Managers/AttendanceManager.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+public class AttendanceManager : Singleton<AttendanceManager>
+{
+    private const string LOGINDATE_KEY = "LastLoginDate";
+    private const string ATTENDANCE_COUNT_KEY = "AttendanceCount";
+
+    // 관리자 : 추가, 삭제, 조회, 정렬
+    [SerializeField]
+    private List<AttendanceDataSO> _soDatas;
+
+    // 출석 데이터로 만든 출석 객체들
+    private List<Attendance> _attendances;
+    // 조회
+    public List<Attendance> Attendances => _attendances;
+
+    //출석 검증 데이터
+    private DateTime _lastLoginDateTime = new DateTime(); //마지막 로그인 날짜
+    private int _attendanceCount = 0; // 출석 횟수
+
+    public Action OnDataChanged;
+
+    private void Awake()
+    {
+        //추가
+        //게임 시작할 때 출석 객체 생성(기획자가 세팅한 만큼)
+        _attendances = new List<Attendance>(_soDatas.Count); //최적화
+        foreach (AttendanceDataSO data in _soDatas)
+        {
+            Attendance attendance = new Attendance(data, false);
+            _attendances.Add(attendance);
+        }
+        Load();
+        AttendanceCheck();
+        StartCoroutine(SaveDate());
+    }
+
+    private void Load()
+    {
+        string dateString = PlayerPrefs.GetString(LOGINDATE_KEY, DateTime.Today.ToString("yyyy-MM-dd"));
+        _lastLoginDateTime = DateTime.ParseExact(dateString, "yyyy-MM-dd", null);
+
+        _attendanceCount = PlayerPrefs.GetInt(ATTENDANCE_COUNT_KEY, 0);
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetString(LOGINDATE_KEY, DateTime.Today.ToString());
+        PlayerPrefs.SetInt(ATTENDANCE_COUNT_KEY, _attendanceCount);
+    }
+
+    private IEnumerator SaveDate()
+    {
+        while(true)
+        {
+            Save();
+            yield return new WaitForSeconds(60);
+        }
+    }
+
+    private void AttendanceCheck()
+    {
+        DateTime today = DateTime.Today;
+        if(today > _lastLoginDateTime)
+        {
+            _lastLoginDateTime = today;
+            _attendanceCount++;
+        }
+    }
+
+    //보상 받기
+    public bool TryGetReward(Attendance attendance)
+    {
+        // 조건 1 : 이미 보상 받았다면 실패
+        if(attendance.IsRewarded)
+        {
+            return false;
+        }
+
+        //조건 2 : 실제 그만큼 출석 했는가
+        if(_attendanceCount < attendance.Data.Day)
+        {
+            return false;
+        }
+
+        attendance.IsRewarded = true;
+        CurrenyManager.Instance.Add(attendance.Data.RewardCurrencyType, attendance.Data.RewardAmount);
+
+        OnDataChanged?.Invoke();
+
+        return true;
+    }
+}
